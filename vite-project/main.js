f276c01e1bdce0ea1b65e7f2586ebc68b4697d14
ttaylordev <ttaylordev@gmail.com>
2021-10-07 14:45:19 -0600

    donut animated on three axis

--- vite-project/main.js
+++ vite-project/main.js
@@ -12,31 +12,45 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'), 
 });
-
+// changed something
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
-// end of scene, camera, renderer setup
-
+// /* end of scene, camera, renderer setup */
 
 // Geometry
 // x,y,z set of vectors that define the shape
-
-const geometry = new THREE.TorusGeometry(10,2,18,100);
+const geometry = new THREE.TorusGeometry(10,2,15,100);
 
 // Material
 // give it some color/texture
 // there are many predefined materials, custom shaders(WebGL) are great too.
-const material = new THREE.MeshBasicMaterial({color: 0xFF8347, wireframe: true});
 // basic materials do not require a lightsource
+const material = new THREE.MeshBasicMaterial({color: 0xFF8347, wireframe: true});
 
 // The mesh is the combination of geometry and material
 const torus = new THREE.Mesh(geometry, material);
 
-// add it to the scene
+// add the torus to the scene
 scene.add(torus);
 
-// render to the DOM statically
-renderer.render(scene, camera);
+// scene can be rendered to the DOM statically, but avoid calling the render method frequently
+// renderer.render(scene, camera);
+
+// "game loop" recursion, rerenders the browser and animates the subject
+function animate(){
+  // tells the browser to call the function everytime it repaints the screen
+  requestAnimationFrame( animate );
+
+  // each shape has properties, rotation, shape, scale
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
